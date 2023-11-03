1a18d25cb9714fe5a32b8ac409c153d2af9e8f9a
frombread <gon03004@gmail.com>
2023-11-04 01:03:40 +0900

    first setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientModule } from './patient/patient.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'process';
 
 @Module({
-  imports: [PatientModule],
+  imports: [PatientModule, MongooseModule.forRoot(process.env.MONGO_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
