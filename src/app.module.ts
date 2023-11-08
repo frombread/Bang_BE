4764f67a2b95173cdfd9a036a6db5dae93829f2b
frombread <gon03004@gmail.com>
2023-11-09 01:31:09 +0900

    도커 실행

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PatientsModule } from './patients/patients.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PatientsModule,
-    ConfigModule.forRoot({
-      envFilePath: ['.development.env'], // 파일 이름을 명시합니다.
-    }),
-    MongooseModule.forRoot(process.env.MONGO_URL),
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/humanscape'),
   ],
   controllers: [AppController],
   providers: [AppService],
